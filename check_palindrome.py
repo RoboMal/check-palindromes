522f930a0b2eac5be9a161661647cad0e1b10358
Malcolm King <malcolm.elijah.king@gmail.com>
2024-02-07 14:38:17 -0600

    Refactored palindrome check to use a function

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,7 +1,9 @@
-text = "racecar"
+def check_palindrome(text):
+    if text == text[::-1]:
+        print(f"{text} is a palindrome")
+    else:
+        print(f"{text} is not a palindrome")
 
-if text == text[::-1]:
-    print(f"{text} is a palindrome")
-else:
-    print(f"{text} is not a palindrome")
+text = "racecar"
+check_palindrome(text)
 
